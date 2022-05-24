70e25910aa3aab07be47b8b3100e7c8e05d9250e
lucas501 <lucas.lelann@gmail.com>
2022-05-24 13:29:10 +0000

    first commit

--- workspace.R
+++ workspace.R
@@ -1,17 +1,43 @@
 library(tercen)
 library(dplyr, warn.conflicts = FALSE)
+library(tidyr)
+library(reshape)
+library(Seurat)
+library(HGNChelper)
+source("https://raw.githubusercontent.com/IanevskiAleksandr/sc-type/master/R/gene_sets_prepare.R")
+source("https://raw.githubusercontent.com/IanevskiAleksandr/sc-type/master/R/sctype_score_.R")
+# load auto-detection function
+#source("https://raw.githubusercontent.com/IanevskiAleksandr/sc-type/master/R/auto_detect_tissue_type.R")
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "bb1b63cf1a12a9aabb0ce7b33d018aee")
+options("tercen.stepId"     = "bdf24f26-8dd1-4cca-ab9c-80a09c2a6a64")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
-ctx = tercenCtx()
+# load database
+db_ = "https://raw.githubusercontent.com/IanevskiAleksandr/sc-type/master/ScTypeDB_full.xlsx"
+tissue = "Immune system" # e.g. Immune system, Liver, Pancreas, Kidney, Eye, Brain
 
-ctx %>%
-  select(.y, .ci, .ri) %>%
-  group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
+ctx <- tercenCtx()
+scRNAseqData<-as.matrix(ctx)
+
+#.ri <- seq(from = 0, to = length(as.matrix(rselect(ctx))) - 1)
+#.ci <- seq(from = 0, to = length(as.matrix(cselect(ctx))) - 1)
+#rownames(scRNAseqData)<-.ri 
+#colnames(scRNAseqData)<-.ci
+rownames(scRNAseqData)<-as.matrix(rselect(ctx))
+colnames(scRNAseqData)<-as.matrix(cselect(ctx))
+
+#scRNAseqData <- read.csv("./pbmc3k.csv",row.names = 1)
+# prepare gene sets
+gs_list <- gene_sets_prepare(db_, tissue)
+# assign cell types
+es.max <- sctype_score(scRNAseqData = scRNAseqData, scaled = TRUE, gs = gs_list$gs_positive, gs2 = gs_list$gs_negative)
+
+es.max.long <- melt(es.max)
+df_test<-data.frame(.ci = seq(from=0,to=length(es.max.long)-1), f=es.max.long) 
+df_test %>%
   ctx$addNamespace() %>%
   ctx$save()
+
